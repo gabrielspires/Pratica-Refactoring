1c36c92955b63d6741e76ba7bf9faa759ca3ede7
Gabriel P <gabrielpires@dcc.ufmg.br>
2019-11-11 14:24:00 -0300

    Commit 9

--- src/Rental.java
+++ src/Rental.java
@@ -15,30 +15,10 @@ class Rental {
 
     //determine amounts for each line
     double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _movie._price.getCharge(_daysRented);
     }
 
-    // add bonus for a two day new release rental
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie._price.getFrequentRenterPoints(_daysRented);
     }
 }
